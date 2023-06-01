7b3e306bea50b9d1d767e36ac7a3dec076318ba8
Kkssrr <109816139+Kkssrr@users.noreply.github.com>
2023-06-01 12:41:49 +0530

    Update and rename info (3).py to info.py

--- info.py
+++ info.py
@@ -42,8 +42,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://pritamgang4:WE80Y5BTAеpcSmWP@cluster0.zjiqwba.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "smart")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Real:QICMgfw8fdbfst41@cluster0.i4idyao.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Real")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
